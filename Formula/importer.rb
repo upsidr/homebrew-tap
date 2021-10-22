6555a9e83d8115082a552f6f8dc23b8d91b7e990
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-22 18:40:57 +0100

    Brew formula update for importer version v0.1.1

--- Formula/importer.rb
+++ Formula/importer.rb
@@ -5,24 +5,27 @@
 class Importer < Formula
   desc "Import any lines, from anywhere"
   homepage "https://github.com/upsidr/importer"
-  version "0.1.0"
-  bottle :unneeded
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/upsidr/importer/releases/download/v0.1.0/importer_0.1.0_darwin_amd64.tar.gz"
-      sha256 "11fb2628406e2edb0254a082d06ad9a20db6a2139a09a9cc6a534b083f894328"
+      url "https://github.com/upsidr/importer/releases/download/v0.1.1/importer_0.1.1_darwin_amd64.tar.gz"
+      sha256 "8d7a77f2c14563a1740e6740a2c335b91326bc42d05c3d736548649aaf85765c"
+
+      def install
+        bin.install "importer"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/upsidr/importer/releases/download/v0.1.0/importer_0.1.0_linux_amd64.tar.gz"
-      sha256 "c817585f936a5de9b0c3651e9d62bb8a3169e1a7cb0f15d64af7f13b9b49e45b"
-    end
-  end
+      url "https://github.com/upsidr/importer/releases/download/v0.1.1/importer_0.1.1_linux_amd64.tar.gz"
+      sha256 "610ec9670ac7dd28e220d64e1d811e1773242235ad4f2989967224d9fb40baa1"
 
-  def install
-    bin.install "importer"
+      def install
+        bin.install "importer"
+      end
+    end
   end
 end
